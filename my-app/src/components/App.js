0bbdac6f562626d3c89213fba9d78c0329c9fcdb
ShevchenkoDmytriy <dimasevcenko506@gmail.com>
2023-09-27 20:52:50 +0300

    React

---
+++ my-app/src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="Back">
+                <div className="Border">
+
+                </div>
+            </div>
+        )
+    }
+}
+export default App
