e52d3d62fa0cbfefc6163e33bd8c870293699219
ShevchenkoDmytriy <dimasevcenko506@gmail.com>
2023-09-28 18:57:54 +0300

    Update App.js

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,16 +1,41 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "./Header"
+
+
+
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="Back">
-                <div className="Border">
-
-                </div>
-            </div>
-        )
+    constructor(props) {
+      super(props);
+      this.state = {
+        seconds: 0,
+      };
     }
-}
+  
+    componentDidMount() {
+      this.interval = setInterval(() => {
+        this.setState((prevState) => ({
+          seconds: prevState.seconds + 1,
+        }));
+      }, 1000);
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.interval);
+    }
+  
+    render() {
+      return (
+        <div>
+          <h1>Timer: {this.state.seconds} seconds</h1>
+        </div>
+      );
+    }
+  }
+  
+    
+
+
+
+
 export default App
